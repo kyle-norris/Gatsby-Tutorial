33ca88db5e9181f47c9a8dfa7871c497dc6c9ae8
kyle-norris <kyle.coding@outlook.com>
2021-01-31 13:55:05 -0500

    Started tutorial from Coding Addict on Youtube

--- portfolio/src/components/layout.js
+++ portfolio/src/components/layout.js
@@ -1,26 +1,15 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import base from './base.css'
-import Container from './container'
-import Navigation from './navigation'
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
-class Template extends React.Component {
-  render() {
-    const { location, children } = this.props
-    let header
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
 
-    let rootPath = `/`
-    if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
-      rootPath = __PATH_PREFIX__ + `/`
-    }
-
-    return (
-      <Container>
-        <Navigation />
-        {children}
-      </Container>
-    )
-  }
-}
-
-export default Template
+export default Layout;
