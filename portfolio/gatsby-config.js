9c3767fb724b3d25a5bbf9e07c6cb79282a04387
kyle-norris <kyle.coding@outlook.com>
2021-01-31 11:58:20 -0500

    Tried a new starter

--- portfolio/gatsby-config.js
+++ portfolio/gatsby-config.js
@@ -1,16 +1,41 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
-module.exports = {
-  /* Your site config here */
-  plugins: [`gatsby-plugin-styled-components`],
-  siteMetadata: {
-    title: "Kyle's Portfolio",
-    author: "Kyle Norris",
-    description: "",
-  }
+const contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 }
 
+// if you want to use the preview API please define
+// CONTENTFUL_HOST in your environment config
+// the `host` property should map to `preview.contentful.com`
+// https://www.contentful.com/developers/docs/references/content-preview-api/#/reference/spaces/space/get-a-space/console/js
+if (process.env.CONTENTFUL_HOST) {
+  contentfulConfig.host = process.env.CONTENTFUL_HOST
+}
+
+const { spaceId, accessToken } = contentfulConfig
+
+if (!spaceId || !accessToken) {
+  throw new Error(
+    'Contentful spaceId and the access token need to be provided.'
+  )
+}
+
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Contentful starter',
+  },
+  pathPrefix: '/gatsby-contentful-starter',
+  plugins: [
+    'gatsby-transformer-remark',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: contentfulConfig,
+    },
+  ],
+}
--- portfolio/src/components/layout.js
+++ portfolio/src/components/layout.js
@@ -1,17 +1,26 @@
-import React, { Children } from "react"
-import { createGlobalStyle } from "styled-components"
+import React from 'react'
+import { Link } from 'gatsby'
+import base from './base.css'
+import Container from './container'
+import Navigation from './navigation'
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    color: ${props => (props.theme === "purple" ? "purple" : "black")};
+class Template extends React.Component {
+  render() {
+    const { location, children } = this.props
+    let header
+
+    let rootPath = `/`
+    if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
+      rootPath = __PATH_PREFIX__ + `/`
+    }
+
+    return (
+      <Container>
+        <Navigation />
+        {children}
+      </Container>
+    )
   }
-`
-
-export default function Layout({ children }) {
-  return (
-    <>
-      <GlobalStyle theme="purple"/>
-      {children}
-    </>
-  )
 }
+
+export default Template
