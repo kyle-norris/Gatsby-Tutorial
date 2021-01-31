a8dca701abe50040b125e4fb3879feee19a547a9
kyle-norris <kyle.coding@outlook.com>
2021-01-31 14:26:25 -0500

    Learned about styling

--- portfolio/src/pages/blog.js
+++ portfolio/src/pages/blog.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Layout from "../components/Layout";
+import styles from "../components/blog.module.css";
 
 const blog = () => {
   return (
     <Layout>
-      <h1>This is our blog page</h1>
+      <div className={styles.page}>
+        <h1>This is our blog page</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est ad iusto
+          laboriosam quibusdam ex, similique, magni enim iure, recusandae minus
+          labore praesentium ratione tempora aspernatur?
+        </p>
+      </div>
     </Layout>
   );
 };
