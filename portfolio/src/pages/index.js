e8beaf69a62fbb666eaf502c33dd13dad730ea2f
kyle-norris <kyle.coding@outlook.com>
2021-01-30 23:59:20 -0500

    Created styled components page

--- portfolio/src/pages/index.js
+++ portfolio/src/pages/index.js
@@ -2,10 +2,11 @@ import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
 
+
 export default function Home() {
   return (
     <div style={{ color: `purple` }}>
-      <Link to="/contact/">Contact</Link>
+      <Link to="/styledComponents">Styled Components</Link>
       <Header headerText="Hello Gatsby!"/>
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
